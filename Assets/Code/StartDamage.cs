ab3103d25d57675239a0b818403cc209e413bd3b
GamesForeva <starotitorovaleksei@gmail.com>
2025-07-20 19:24:01 +0300

    домашняя работа 2
    Для домашней работы №2 добави 2 .cs файла в папке code, прикрепил на обьект code homework. строка Attack для прописывания урона

--- Assets/Code/StartDamage.cs
+++ Assets/Code/StartDamage.cs
@@ -10,10 +10,23 @@ namespace Code
 
     {
         
-
         private void Start()
         {
+            First();
+            
+        }
+        
+        public float Attack;
+        
+        private void OnValidate()
+        {
+            SummaDamage(Attack);
+        }
 
+        
+        
+        private void First()
+        {
             float StartHealth = 100;
             float DamageAfterStart = Random.Range(0, 15f);
             float result = StartHealth - DamageAfterStart;
@@ -22,11 +35,20 @@ namespace Code
             Debug.LogWarning($"Player {result} Health");
         }
 
-        
+        private void SummaDamage(float basedamage)
+        {
+            float BaseHP = 100;
+            
+            float multiplier = Random.Range(1f, 2f);
+            
+            float Damage = (basedamage * multiplier);
+            
+            Debug.LogWarning($"Health {BaseHP - Damage}");
 
 
-
-
-
+        }
+         
     }
+    
+    
 }
